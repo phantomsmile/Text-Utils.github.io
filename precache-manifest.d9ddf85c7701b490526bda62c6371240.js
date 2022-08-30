57c0718ceea61b070e4699b54bd89ae446f29ec0
phantomsmile <47076030+phantomsmile@users.noreply.github.com>
2022-08-30 13:30:34 +0530

    Updates

---
+++ precache-manifest.d9ddf85c7701b490526bda62c6371240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1833b1fa5d9fdf73d74a",
+    "url": "/Text-Utils/static/css/main.2223c602.chunk.css"
+  },
+  {
+    "revision": "1833b1fa5d9fdf73d74a",
+    "url": "/Text-Utils/static/js/main.1833b1fa.chunk.js"
+  },
+  {
+    "revision": "7e907defe05928e8d093",
+    "url": "/Text-Utils/static/js/1.7e907def.chunk.js"
+  },
+  {
+    "revision": "563923fbdd46d6359eb9",
+    "url": "/Text-Utils/static/js/2.563923fb.chunk.js"
+  },
+  {
+    "revision": "5263694195d499d3ce27",
+    "url": "/Text-Utils/static/js/runtime~main.52636941.js"
+  },
+  {
+    "revision": "2e6cf1a52d0c5979233965099795d5bd",
+    "url": "/Text-Utils/index.html"
+  }
+];
